ab4b7f6dcfbb376cca80e0c5e1bd9bd42848f45e
nazarovqurbonali <nazarovqurbonali4@gmail.com>
2025-03-13 08:28:47 +0500

    finish creating layout

--- src/monolith/WebUI/GlobalUsing.cs
+++ src/monolith/WebUI/GlobalUsing.cs
@@ -5,7 +5,6 @@ global using WebUI.Infrastructure.DI;
 global using Microsoft.AspNetCore.Mvc;
 global using Bootstrapper.Extensions.DI;
 global using Application.Extensions.Smtp;
-global using Infrastructure.DataAccess.Seed;
 global using Microsoft.AspNetCore.HttpLogging;
 global using WebUI.Infrastructure.Middlewares;
 global using Microsoft.AspNetCore.Authentication.Cookies;
